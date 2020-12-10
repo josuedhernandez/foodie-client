131b6b354f14dea7eb3a828a276dc39538910004
Josue Hernandez <moradel82@gmail.com>
2020-12-10 01:26:15 -0700

    First routes added

---
+++ src/components/App/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Header from "../Header/Header";
+import LoginPage from "../../routes/LoginPage/LoginPage";
+import RegistrationPage from "../../routes/RegistrationPage/RegistrationPage";
+import NotFoundPage from "../../routes/NotFoundPage/NotFoundPage";
+
+class App extends Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    console.error(error);
+    return { hasError: true };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App__header">
+          <Header />
+        </header>
+        <main className="App__main">
+          {this.state.hasError && (
+            <p className="red">There was an error! Oh no!</p>
+          )}
+          <Switch>
+            <Route exact path={"/"} component={ArticleListPage} />
+            <Route path={"/login"} component={LoginPage} />
+            <Route path={"/register"} component={RegistrationPage} />
+            {/* <Route
+              path={'/article/:articleId'}
+              component={ArticlePage}
+            /> */}
+            <Route component={NotFoundPage} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
