e3c875c65b3a3a6cb8c326c81e1041af4abc7669
Josue Hernandez <moradel82@gmail.com>
2021-03-11 01:28:56 -0700

    Updated README.md and added images

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  // API_ENDPOINT: "https://morning-castle-66587.herokuapp.com/api",
-  API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://morning-castle-66587.herokuapp.com/api",
   TOKEN_KEY: "blogful-client-auth-token",
 };
